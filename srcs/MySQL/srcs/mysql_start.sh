2ee97cfbbf1684e4b856e3d5f90f353295b6579b
Hamza Rhirha <hrhirha@student.1337.ma>
2020-12-09 20:21:08 +0100

    reverse proxy to phpmyadmin is now working

--- srcs/MySQL/srcs/mysql_start.sh
+++ srcs/MySQL/srcs/mysql_start.sh
@@ -7,13 +7,14 @@ echo "Creating DATABASES"
 chown -R mysql: /var/lib/mysql
 /etc/init.d/mariadb start
 mysql -e "CREATE DATABASE wordpress_db;"
-# mysql -e "CREATE DATABASE phpmyadmin_db;"
+mysql -e "CREATE DATABASE phpmyadmin_db;"
 # mysql -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wp_admin'@% IDENTIFIED BY 'wp_admin';FLUSH PRIVILEGES;"
 # mysql -e "GRANT ALL PRIVILEGES ON phpmyadmin_db.* TO 'pma_admin'@% IDENTIFIED BY 'pma_admin';FLUSH PRIVILEGES;"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' IDENTIFIED BY 'admin'; FLUSH PRIVILEGES;"
 
 mysql -e "USE wordpress_db;"
 mysql wordpress_db < /tmp/wordpress_db.sql
+mysql phpmyadmin_db < /tmp/phpmyadmin_db.sql
 /etc/init.d/mariadb stop
 
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
